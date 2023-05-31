872e977543d225b4cc75090cabb4db083c03c986
Влад Симакович <simakovich_vlad228@mail.ru>
2023-05-31 12:40:53 +0300

    add some new logic

---
+++ tour/tour/Models/TicketByUserId.cs
@@ -0,0 +1,16 @@
+﻿using diploma.Db.Tour.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using tour.Db.TourDb.Entities;
+
+namespace tour.Models
+{
+    public class TicketByUserId
+    {
+        public int Id { get; set; }
+        public string? TourName { get; set; }
+        public List<int>? PlaceNumbers { get; set; }
+        public DateTime TourTimeStart { get; set; }
+        public DateTime TourTimeEnd { get; set; }
+
+    }
+}
